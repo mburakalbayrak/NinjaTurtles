4498dff31acd10b07e2a647cf7c94f8215c1c11c
mburakalbayrak <burakalbayrak3434@gmail.com>
2024-11-18 21:26:39 +0300

    added project

---
+++ NinjaTurtles.Business/Abstract/IProductService.cs
@@ -0,0 +1,24 @@
+﻿using NinjaTurtles.Core.Utilities.Results;
+using NinjaTurtles.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaTurtles.Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<Product> GetById(int productId);
+        IDataResult<List<Product>> GetList();
+
+        IDataResult<List<Product>> GetListByCategory(int categoryId);
+
+        IDataResult<Product> Add(Product product);
+
+        IResult Delete(Product product);
+
+        IDataResult<Product> Update(Product product);
+    }
+}

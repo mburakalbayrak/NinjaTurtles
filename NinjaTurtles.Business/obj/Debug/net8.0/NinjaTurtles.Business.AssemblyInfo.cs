70615aaaeb10b4433f5813dc3e8116878095f2b5
mburakalbayrak <burakalbayrak3434@gmail.com>
2024-11-18 21:22:05 +0300

    test

--- NinjaTurtles.Business/obj/Debug/net8.0/NinjaTurtles.Business.AssemblyInfo.cs
+++ NinjaTurtles.Business/obj/Debug/net8.0/NinjaTurtles.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NinjaTurtles.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7f5d231585afd167d495224934887bcc217dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NinjaTurtles.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NinjaTurtles.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

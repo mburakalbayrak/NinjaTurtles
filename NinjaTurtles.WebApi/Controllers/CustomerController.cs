2a2d1d6df3ff3e87d6b4e0df9442f0a74eae39b9
Yusuf Çelik <ysfcelikk@gmail.com>
2024-11-18 23:41:36 +0300

    customer controller eklendi,autofac classları  eklendi

---
+++ NinjaTurtles.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NinjaTurtles.Business.Abstract;
+using NinjaTurtles.Entities.Dtos;
+
+namespace NinjaTurtles.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] AddCustomerDto dto)
+        {
+            var result =_customerService.Add(dto);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var result = _customerService.Delete(id);
+            return Ok(result);
+        }
+    }
+}

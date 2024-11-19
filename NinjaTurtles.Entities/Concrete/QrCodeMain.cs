c4d868b4ae21c9b04462db7161e4ad52dbbedde2
mburakalbayrak <burakalbayrak3434@gmail.com>
2024-11-20 01:29:59 +0300

    commit

--- NinjaTurtles.Entities/Concrete/QrCodeMain.cs
+++ NinjaTurtles.Entities/Concrete/QrCodeMain.cs
@@ -5,7 +5,7 @@ namespace NinjaTurtles.Entities.Concrete
     public class QrCodeMain:BaseEntity<Guid>, IEntity
     {
         public int? CustomerId { get; set; }
-        public string? Url { get; set; }
+        public string? RedirectUrl { get; set; }
         public string? ImageUrl { get; set; }
         public int? QrDetailId { get; set; }
         public bool IsDeleted { get; set; }

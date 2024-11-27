fcf1f69393a8ae16abbc472415c77098e252cfad
mburakalbayrak <burakalbayrak3434@gmail.com>
2024-11-27 20:04:55 +0300

    FluentValidation eklendi

---
+++ NinjaTurtles.Business/Constants/ValidationMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaTurtles.Business.Constants
+{
+    public static class ValidationMessages
+    {
+        public static string CustomerEmailNotEmpty = "Email boş geçilemez";
+
+    }
+}
---
+++ NinjaTurtles.Business/ValidationRules/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using NinjaTurtles.Business.Constants;
+using NinjaTurtles.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaTurtles.Business.ValidationRules.FluentValidation
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(c => c.Email).NotEmpty().WithMessage(ValidationMessages.CustomerEmailNotEmpty);
+            RuleFor(c => c.Email).EmailAddress();
+            RuleFor(c => c.PhoneNumber).NotEmpty();
+            RuleFor(c => c.PhoneNumber).Matches(@"^\+90\d{10}$").WithMessage("Telefon numarası geçerli bir formatta olmalıdır. (+90XXXXXXXXXX)");
+
+        }
+    }
+}

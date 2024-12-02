d26161995e3c58603bc9db3d6fe9bf4fc8a6dc1c
Yusuf Çelik <ysfcelikk@gmail.com>
2024-12-02 18:03:24 +0300

    qrcontroller eklendi.

---
+++ NinjaTurtles.WebApi/Controllers/QrController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NinjaTurtles.Business.Abstract;
+using NinjaTurtles.Entities.Dtos;
+
+namespace NinjaTurtles.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrController : Controller
+    {
+        private IQrService _qrService;
+
+        public QrController(IQrService qrService)
+        {
+            _qrService = qrService;
+        }
+
+        [HttpGet("GetQrDetail")]
+        public IActionResult GetQrDetail(Guid id)
+        {
+            var qr = _qrService.GetQrDetail(id);
+            return Ok(qr);
+        }
+
+        [HttpPost]
+        public IActionResult CreateHumanDetail([FromBody] QrCodeHumanCreateDto dto)
+        {
+            var result = _qrService.CreateHumanDetail(dto);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAnimalDetail([FromBody] QrCodeAnimalCreateDto dto)
+        {
+            var result = _qrService.CreateAnimalDetail(dto);
+            return Ok(result);
+        }
+
+        [HttpGet("GetCurrentDirectory")]
+        public IActionResult GetCurrentDirectory()
+        {
+            var currentdirectory = Directory.GetCurrentDirectory();
+            return Ok(currentdirectory);
+        }
+    }
+}

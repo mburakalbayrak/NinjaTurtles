6e5701a34d47cd5e46e0386f95e7f6fef77438df
Yusuf Çelik <ysfcelikk@gmail.com>
2024-12-26 23:09:01 +0300

    animal için migration yapıldı

--- NinjaTurtles.DataAccess/Migrations/20241226200614_mig-10.Designer.cs
+++ NinjaTurtles.DataAccess/Migrations/20241226200614_mig-10.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NinjaTurtles.DataAccess.Concrete.EntityFramework.Contexts;
 
@@ -11,9 +12,11 @@ using NinjaTurtles.DataAccess.Concrete.EntityFramework.Contexts;
 namespace NinjaTurtles.DataAccess.Migrations
 {
     [DbContext(typeof(NinjaTurtlesContext))]
-    partial class NinjaTurtlesContextModelSnapshot : ModelSnapshot
+    [Migration("20241226200614_mig-10")]
+    partial class mig10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
